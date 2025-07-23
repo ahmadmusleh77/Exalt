20370f7c5054f0e346af2430d06a1662fabcb10f
ahmad musleh <s12217787@stu.najah.edu>
2025-07-23 09:40:32 +0300

    add class person

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -38,6 +38,7 @@ public class Person {
     public static void main(String[] args) {
      Person person = new Person("John Doe", 20, "Male");
      System.out.println(person.getName());
+     //hello
     }
 }
 
